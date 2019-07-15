8664c6c4cf86a5ea7543decb915917371f8031ae
EC2 Default User <ec2-user@ip-172-31-33-67.ap-northeast-1.compute.internal>
2019-07-15 04:23:20 +0000

    build minimum function

---
+++ qanda/app/controllers/questions_controller.rb
@@ -0,0 +1,42 @@
+class QuestionsController < ApplicationController
+  def index
+  	@questions = Question.all
+  end
+
+  def show
+  end
+
+  def new
+  	@question = Question.new
+  end
+  
+  def create
+  	@question = Question.new(question_params)
+    if @question.save
+  	  redirect_to root_path, notice: 'Success!'
+    else
+  	  flash[:alert] = 'Save error!'
+  	  render :new
+    end
+  end
+
+  def edit
+  	@question = Question.find(params[:id])
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+    	redirect_to root_path, notice: 'Success'
+    else
+      flash[:alert] = 'Save error'
+      render :edit
+    end
+  end
+  
+  private
+    def question_params
+      params.require(:question).permit(:name, :title, :content)	
+    end
+
+end
--- qanda/db/seeds.rb
+++ qanda/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1, name: 'Test name 1', title: 'Test questionsuestion 1', content: 'Test Content')
+Question.create(id: 2, name: 'Test name 2', title: 'Test questionsuestion 2', content: 'Test Content')
+Question.create(id: 3, name: 'Test name 3', title: 'Test questionsuestion 3', content: 'Test Content')
