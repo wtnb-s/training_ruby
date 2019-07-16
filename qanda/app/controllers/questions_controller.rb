0adce0ef2636c125a6575345ffe1041c284285c4
EC2 Default User <ec2-user@ip-172-31-33-67.ap-northeast-1.compute.internal>
2019-07-16 14:48:03 +0000

    add Answer Post function

--- qanda/app/controllers/questions_controller.rb
+++ qanda/app/controllers/questions_controller.rb
@@ -1,9 +1,11 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   def index
   	@questions = Question.all
   end
 
   def show
+    @answer = Answer.new
   end
 
   def new
@@ -21,11 +23,9 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-  	@question = Question.find(params[:id])
   end
   
   def update
-    @question = Question.find(params[:id])
     if @question.update(question_params)
     	redirect_to root_path, notice: 'Success'
     else
@@ -34,9 +34,16 @@ class QuestionsController < ApplicationController
     end
   end
   
+  def destroy
+    @question.destroy
+    redirect_to root_path, notice: 'Success'
+  end
+  
   private
+    def set_question
+    @question = Question.find(params[:id])
+    end
     def question_params
       params.require(:question).permit(:name, :title, :content)	
     end
-
 end
--- qanda/config/routes.rb
+++ qanda/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'answers/edit'
   root 'questions#index'
-  resources :questions
+  resources :questions do
+  	resources :answers
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
